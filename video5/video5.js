b6753b188510401255e6d1493248b7dd4f71fa66
DuongN <roxduong169@gmail.com>
2023-05-24 15:03:21 +0700

    string methods

---
+++ video5/video5.js
@@ -0,0 +1,16 @@
+//JavaScript String Length
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;
+console.log(length);
+//JavaScript String slice()
+let sliceString = 'Apple, Banana, kiwi';
+let part = sliceString.slice(7, 13);
+console.log(part);
+//JavaScript String substring()
+let str = "Apple, Banana, Kiwi";
+let part1 = str.substring(7, 13);
+console.log(part1);
+//JavaScript String substr()
+let str1 = "Apple, Banana, Mango";
+let part2 = str1.substr(7, 6);
+console.log(part2);
