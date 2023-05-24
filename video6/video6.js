7b4ec12de820e960831a7d159fb222ffa2d0f4a5
DuongN <roxduong169@gmail.com>
2023-05-24 18:25:21 +0700

    string template

--- video6/video6.js
+++
@@ -1,8 +0,0 @@
-let x = 0.2 + 0.1;
-
-console.log(x);
-let a = 5
-let b = '5'
-console.log(a + b, a-b, a*b, a/b)
-let c = +b;
-console.log( a === c, 'a: ', typeof a, 'b: ', typeof b, 'c: ', typeof c )
